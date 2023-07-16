cbde76e617cbb3866335dddc7e79977a3deb69f8
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-07-16 14:07:31 -0300

    Adicionei a página que irá ser transmitida.

---
+++ Plant-Status/Main_Page.h
@@ -0,0 +1,25 @@
+const char MAIN_PAGE[] PROGMEM = R"=====(
+
+<!DOCTYPE html>
+
+<html lang="pt-BR">
+
+	<head>
+		<title>Teste</title>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+	</head>
+
+	<body>
+		<header>
+			<h1>Hello World!</h1>
+		</header>
+
+		<main>
+			<p>Again!</p>
+		</main>
+
+	</body>
+
+</html>
+)=====";
