64407f93db4e92f0b6f87555478ce990b49bb474
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-07-30 20:49:49 -0300

    Adicionei o javascript necessário para mostrar na tela a umidade do solo.

--- Plant-Status/Main_Page.h
+++ Plant-Status/Main_Page.h
@@ -11,13 +11,33 @@ const char MAIN_PAGE[] PROGMEM = R"=====(
 	</head>
 
 	<body>
+
 		<header>
 			<h1>Hello World!</h1>
 		</header>
 
 		<main>
-			<p>Again!</p>
+			<p id="paragrafo">Umidade: </p>
 		</main>
+
+   <script type="text/javascript">
+      const xml_http = new XMLHttpRequest();
+
+      xml_http.open("GET", "/xml");
+      xml_http.send();
+
+      // when the page finishes loading, do this:
+      xml_http.onload = () => {
+        if(xml_http.readyState === xml_http.DONE || xml_http.status === 200) {
+          const moisture = Number(xml_http.responseXML.firstElementChild.children[0].innerHTML);
+
+        // appends moisture_value to the tag with the correspondent ID
+          document.getElementById("paragrafo").innerHTML += moisture;
+        }
+      }
+
+      console.log("Got XMl!");
+   </script>
 
 	</body>
 
