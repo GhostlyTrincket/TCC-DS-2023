a650b14aad5df8e6e5dd5224668ae3ce96973fb1
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-08-12 11:47:30 -0300

    Resumidamente: Corrigi a indentação cagada da IDE do arduino. Adicionei algumas funções para pegar e atualizar as informações (tudo vindo deste repo: https://github.com/KrisKasprzak/ESP32_WebPage/blob/main/WebPageUpdate.ino)

--- Plant-Status/Main_Page.h
+++ Plant-Status/Main_Page.h
@@ -1,7 +1,6 @@
 const char MAIN_PAGE[] PROGMEM = R"=====(
 
 <!DOCTYPE html>
-
 <html lang="pt-BR">
 
 	<head>
@@ -17,29 +16,78 @@ const char MAIN_PAGE[] PROGMEM = R"=====(
 		</header>
 
 		<main>
-			<p id="paragrafo">Umidade: </p>
+			<p>Umidade: <span id="moisture"></span></p>
+			<button onclick="update_moisture()">Clique em mim para iniciar</button>
 		</main>
 
-   <script type="text/javascript">
-      const xml_http = new XMLHttpRequest();
+		<script type="text/javascript">
+			const xml_http = new XMLHttpRequest();
 
-      xml_http.open("GET", "/xml");
-      xml_http.send();
+			xml_http.open("GET", "/xml");
 
-      // when the page finishes loading, do this:
-      xml_http.onload = () => {
-        if(xml_http.readyState === xml_http.DONE || xml_http.status === 200) {
-          const moisture = Number(xml_http.responseXML.firstElementChild.children[0].innerHTML);
+			function update_moisture() {
+				const update_request = new XMLHttpRequest();
+				const xml_response = xml_http.responseXML;
 
-        // appends moisture_value to the tag with the correspondent ID
-          document.getElementById("paragrafo").innerHTML += moisture;
+				update_request.onreadystatechange = () => {
+					if(update_request.readyState == 4 && update_request.status == 200) {
+						const real_time_moisture = Number(update_request.responseText);
+
+						document.getElementById("moisture").innerHTML = real_time_moisture;
+					}
+				}
+
+				update_request.open("PUT", "/update_moisture?value="+Number(xml_response.getElementsByTagName("Moisture")[0].innerHTML, true))
+				update_request.send();
+			}
+
+			function respond_to_process() {
+        // const request = new XMLHttpRequest();
+
+        // request.open("GET", "/xml");
+
+				const bad_soil = 1230;
+				const good_soil = 2460;
+				const very_good_soil = 3690;
+
+				const xml_response = xml_http.responseXML;
+				const xml_document = Number(xml_response.getElementsByTagName("Moisture"));
+
+				let moisture = xml_document[0].innerHTML;
+
+				console.log(moisture);
+
+        if(moisture <= 0 && moisture <= bad_soil) {
+          console.log("the soil is bad");
+        } else if(moisture > bad_soil && moisture <= good_soil) {
+          console.log("the soil is good");
+        } else if(moisture > good_soil && moisture <= very_good_soil) {
+          console.log("the soil is perfect.");
         }
-      }
 
-      console.log("Got XMl!");
-   </script>
+        // request.send(null);
+			}
+
+			function process() {
+				//alter `time_out`'s to a bigger value, if the page get big.
+				const time_out = 100;
+
+				if(xml_http.readyState == 0 || xml_http.readyState == 4) {
+					xml_http.open("PUT", "/xml", true);
+
+					xml_http.onreadystatechange = respond_to_process;
+
+					xml_http.send(null);
+				}
+
+				setTimeout("process()", time_out);
+			}
+
+			xml_http.send();
+
+		</script>
 
 	</body>
-
 </html>
+
 )=====";
