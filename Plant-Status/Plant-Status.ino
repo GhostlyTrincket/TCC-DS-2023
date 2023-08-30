4e501cf1159f737c3002f51f808672462c4c7cb1
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-08-30 15:56:38 -0300

    Eu resumi alguns comentários, adicionei por acidente espaço em branco, criei um branch novo para mergear mais tarde com a main.

--- Plant-Status/Plant-Status.ino
+++ Plant-Status/Plant-Status.ino
@@ -10,19 +10,17 @@
 //#define PRODUCTION
 
 // variables to create an access point
-IPAddress local_ip(192,168,1,180);
-IPAddress gateway(192,168,2,227);
-IPAddress subnet(255,255,255,0);
+IPAddress local_ip(192,168,1,112);IPAddress gateway(192,168,2,227);IPAddress subnet(255,255,255,0);
 
 WebServer server(80);
 
-char xml[2048]; // buffer for XML operations
-char buffer[64]; // buffer to make operations
+char xml[2048]; // buffer for XML operationschar
+buffer[64]; // buffer to make operations
 
 int soil_moisture = 0;
-int processed_moisture = 0;
+//int processed_moisture = 0;
 
-// Theses values are for calibrating the sensor.
+// values to calibrate the sensor
 const int air_value = 2753;
 const int water_value = 3111;
 
@@ -33,22 +31,29 @@ void setup() {
 #ifndef PRODUCTION
 	init_wifi();
 #endif
-
 #ifdef PRODUCTION
 	init_ap();
 #endif
 	init_routes();
+
 	server.begin();
 }
 
 void loop() {
-	soil_moisture = analogRead(SENSOR_PIN);
-	processed_moisture = map(soil_moisture, air_value, water_value, 0, 100);
+	//	soil_moisture = analogRead(SENSOR_PIN);
+	//	processed_moisture = map(soil_moisture, air_value, water_value, 0, 100);
+	soil_moisture += 1;
 
+	if(soil_moisture >= 100)
+		soil_moisture = 0;
+	
 	server.handleClient();
 }
 
 void init_wifi() {
+	if(!WiFi.config(local_ip, gateway, subnet))
+		Serial.println("Failure to configurate STA device.");
+	
 	WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
 
 	while(WiFi.status() != WL_CONNECTED) {} // while it's not connected to wifi, try to connect to it.
@@ -64,7 +69,6 @@ void init_ap() {
 	Serial.println(WiFi.softAPIP());
 }
 
-
 void init_routes() {
 	server.on("/", send_website);
 	server.on("/xml", send_xml);
@@ -78,18 +82,17 @@ void send_website() {
 void send_xml() {
 	strcpy(xml, "<?xml version = '1.0'?>\n<Data>\n"); // xml header
 
-	sprintf(buffer, "<Moisture>%d</Moisture>", processed_moisture);
-	strcat(xml, buffer);
-
-	strcat(xml, "</Data>\n");
+	sprintf(buffer, "<Moisture>%d</Moisture>", soil_moisture);
+	//	sprintf(buffer, "<Moisture>%d</Moisture>", processed_moisture);
+	strcat(xml, buffer);strcat(xml, "</Data>\n");
 	server.send(200, "text/xml", xml);
 }
 
 void update_moisture() {
-	String server_arg = server.arg("value"); // creates a arg to be used in the URL
+	String server_arg = server.arg("value"); // argument to be used in the URL
 
 	strcpy(buffer, "");
-	sprintf(buffer, "%d", processed_moisture);
+	sprintf(buffer, "%d", soil_moisture);
 	sprintf(buffer, buffer);
 
 	server.send(200, "text/plain", buffer);
