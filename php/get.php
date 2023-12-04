cb0496a38c3262c89b4621a53a0adeb046ccc7ec
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-12-04 18:32:01 -0300

    Corrigi uma query
    Corrigi uma burrada minha na query de obter o valor da umidade (adicionei o "DESC" próximo ao "LIMIT 1")

--- php/get.php
+++ php/get.php
@@ -4,7 +4,7 @@
 
 	$mysql = connect();
 	
-	$select_last_row = "SELECT id,moisture FROM tbl_moisture ORDER BY id LIMIT 1";
+	$select_last_row = "SELECT id, moisture FROM tbl_moisture ORDER BY id DESC LIMIT 1";
 	$sel_last_row_result = $mysql->query($select_last_row);
 
 	$last_row = $sel_last_row_result->fetch_assoc();
